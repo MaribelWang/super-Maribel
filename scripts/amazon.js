06c2e4d4c2f06ac7d5bff04615775fc650a4e0de
Jingwei Wang <paradox.xiyan@gmail.com>
2024-01-01 18:19:08 +0100

    Import products from products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
 let productsHTML = '';
 products.forEach((product)=>{
     productsHTML += `
@@ -76,6 +78,11 @@ document.querySelectorAll('.js-add-to-cart')
                 quantity:1
               });
           }
-          console.log(cart);
+          let cartQuantity = 0;
+          cart.forEach((item)=>{
+            cartQuantity += item.quantity;
+          });
+
+          document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 });
 });  
