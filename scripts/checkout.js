b55519132d242075bd49a6e437799e98cf8e8b47
Jingwei Wang <paradox.xiyan@gmail.com>
2024-01-02 22:21:59 +0100

    Delete products from HTML when clicking delte

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,9 +13,9 @@ cart.forEach((cartItem)=>{
         }
     });
 
-    console.log(matchingProduct);
+ 
     cartSummaryHtml += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -100,6 +100,8 @@ document.querySelectorAll('.js-delete-quantity-link')
     link.addEventListener('click',() =>{
         const productId = link.dataset.productId;
         removeFromCart(productId);
-        console.log(cart);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     });
 });
